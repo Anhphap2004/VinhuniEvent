0b0d183a9b9c07d9a8cc31a81e1fb7148bb8b376
Pháp Nguyễn Bá <phapmeo2k4@gmail.com>
2025-12-13 11:05:02 +0700

    Thông Tin Cá Nhân

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VinhuniEvent.Models;
+namespace VinhuniEvent.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            var user = _context.Users
+             .Include(u => u.EventRegistrations)
+                 .ThenInclude(er => er.Event)
+             .Include(u => u.Attendances)
+                 .ThenInclude(a => a.Event)
+             .FirstOrDefault(u => u.UserId == userId);
+
+            return View(user);
+        }
+    }
+}
